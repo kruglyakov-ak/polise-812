1bf2a55f08e4b034c324ebcc8ecdcdd8007a49ab
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-23 01:00:08 +0300

    add users loader

--- src/views/UsersListView.vue
+++ src/views/UsersListView.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <UsersList :users="users" />
+    <LoaderSpiner v-if="loading" />
+    <UsersList v-else-if="users.length" :users="users" />
+    <h1 v-else>Список пользователей пуст</h1>
   </div>
 </template>
 
 <script>
 import UsersList from "@/components/UsersList.vue";
+import LoaderSpiner from "@/components/LoaderSpiner.vue";
+
 export default {
-  components: { UsersList },
+  components: { UsersList, LoaderSpiner },
   data() {
     return {
       users: [],
@@ -25,4 +29,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  width: 100%;
+  text-align: center;
+  background-color: $white;
+  box-shadow: 0 2px 8px 0 rgba(35, 47, 53, 0.09);
+  padding: 50px;
+
+  @media (max-width: 800px) {
+    padding: 20px;
+  }
+}
+</style>
