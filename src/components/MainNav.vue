3eec4fb7290a2635f430ecbe8ddf12248c5e159a
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-24 17:47:02 +0300

    create conponent

---
+++ src/components/MainNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <li>
+      <router-link :to="{ name: 'usersList' }"
+        >К списку пользователей
+      </router-link>
+    </li>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
