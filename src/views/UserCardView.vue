ec47e0bc7e2e5bf256bb18eb768171c7845aab10
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-24 17:46:48 +0300

    create component

---
+++ src/views/UserCardView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="user">
+    <MainNav />
+  </div>
+</template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+  components: {
+    MainNav,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.user {
+  width: 90vw;
+  height: 90vh;
+  background-color: $white;
+  box-shadow: 0 2px 8px 0 rgba(35, 47, 53, 0.09);
+}
+</style>
