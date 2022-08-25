ccca249b668edffcd394e26ce302d0b8b6cfa564
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-25 22:58:38 +0300

    rename component

---
+++ src/components/UserCard/UserCardPosts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="post in posts" :key="post.id">
+        <p>{{ post.title }}</p>
+        <p>{{ post.body }}</p>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
