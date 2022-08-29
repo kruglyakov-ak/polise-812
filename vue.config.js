d145054b8337c7dadfb788dd0d3b00ae65b6996f
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-29 18:42:56 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/police-812/" : "/",
   transpileDependencies: ["vuetify"],
   css: {
     loaderOptions: {
